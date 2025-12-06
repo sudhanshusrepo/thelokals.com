5484366a33a5ed00e65477a809d1600c8683a364
Sudhanshu <sudhanshu@tutlabs.com>
2025-12-07 02:36:04 +0530

    fix(provider): downgrade router to v6 and dedupe react to fix crash

--- packages/provider/vite.config.ts
+++ packages/provider/vite.config.ts
@@ -1,9 +1,12 @@
 
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     server: {
       port: Number(process.env.PORT) || 5173,
@@ -11,11 +14,16 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     resolve: {
+      dedupe: ['react', 'react-dom', 'react-router-dom'],
       alias: {
         '@': path.resolve(__dirname, '.'),
         '@core': path.resolve(__dirname, '../core'),
         '@thelocals/core': path.resolve(__dirname, '../core'),
       }
+    },
+    define: {
+      // Expose env variables to client
+      'process.env': env
     }
   };
 });
