64777090943c05e97c905c789ef3dda911d64867
Sudhanshu <sudhanshu@tutlabs.com>
2025-12-19 00:13:21 +0530

    fix(build): remove turbopack config to usage of default webpack bundler

--- apps/web-admin/next.config.ts
+++ apps/web-admin/next.config.ts
@@ -4,9 +4,6 @@ import path from "path";
 const nextConfig: NextConfig = {
   transpilePackages: ['@thelocals/core'],
   outputFileTracingRoot: path.join(__dirname, "../../"),
-  turbopack: {
-    root: path.join(__dirname, "../../"),
-  },
 };
 
 export default nextConfig;
