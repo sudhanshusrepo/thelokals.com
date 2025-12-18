4b6f61cd0918969cc32dd988306fcf2ffc68fc57
Sudhanshu <sudhanshu@tutlabs.com>
2025-12-19 01:05:33 +0530

    fix(build): add dummy webpack config to force disable custom turbopack execution

--- apps/web-client/next.config.js
+++ apps/web-client/next.config.js
@@ -4,6 +4,9 @@ const path = require("path");
 const nextConfig = {
   transpilePackages: ['@thelocals/core'],
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  webpack: (config) => {
+    return config;
+  },
 };
 
 module.exports = nextConfig;
