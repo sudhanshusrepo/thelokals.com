edb6140afcfe554cc6bca480399f1f2bbf09c111
Sudhanshu <sudhanshu@tutlabs.com>
2025-12-21 01:06:48 +0530

    fix(provider): enforce edge runtime in not-found and global-error pages

--- apps/web-provider/src/app/global-error.tsx
+++ apps/web-provider/src/app/global-error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 export const dynamic = 'force-dynamic';
+export const runtime = 'edge';
 
 export default function GlobalError({
     error,
--- apps/web-provider/src/app/not-found.tsx
+++ apps/web-provider/src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 // export const runtime = 'edge';
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 export default function NotFound() {
