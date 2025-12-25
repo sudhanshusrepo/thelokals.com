4492448b1190d989828cf955a0126582314eba00
Sudhanshu <sudhanshu@tutlabs.com>
2025-12-25 18:35:36 +0530

    fix(web-admin): remove outputFileTracingRoot to fix cloudflare path resolution

--- apps/web-admin/next.config.js
+++ apps/web-admin/next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@thelocals/core'],
-  outputFileTracingRoot: path.join(__dirname, "../../"),
+  transpilePackages: ['@thelocals/core'],
 
 };
 
