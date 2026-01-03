90ab9c30b771801cee389cbc1c2a409ce1ed3ac6
unknown <881dhan@gmail.com>
2026-01-04 02:15:38 +0530

    fix(next-config): move outputFileTracingRoot to top-level config

--- frontend/new_apps/web-provider/next.config.js
+++ frontend/new_apps/web-provider/next.config.js
@@ -3,9 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@thelocals/core'],
-    experimental: {
-        outputFileTracingRoot: path.join(__dirname, '../../..'),
-    }
+    outputFileTracingRoot: path.join(__dirname, '../../..'),
 };
 
 module.exports = nextConfig;
