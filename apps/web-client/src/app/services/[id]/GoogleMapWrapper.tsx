af267ff8cbba8e9fdd5c5803a04cfd8a2b64764c
unknown <881dhan@gmail.com>
2026-01-13 01:47:27 +0530

    Fix: Lint error in GoogleMapWrapper icon

--- apps/web-client/src/app/services/[id]/GoogleMapWrapper.tsx
+++ apps/web-client/src/app/services/[id]/GoogleMapWrapper.tsx
@@ -28,12 +28,12 @@ export function GoogleMapWrapper() {
                         lng: center.lng + getRandomOffset()
                     }}
                     icon={{
-                        path: google.maps.SymbolPath.CIRCLE,
-                        scale: 8,
+                        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
                         fillColor: '#00C853',
                         fillOpacity: 1,
                         strokeWeight: 2,
                         strokeColor: '#ffffff',
+                        scale: 1,
                     }}
                 />
             ))}
