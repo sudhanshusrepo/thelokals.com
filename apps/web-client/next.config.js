4a1ab2c81007c423e60d80de0b3d5ef4999728aa
unknown <881dhan@gmail.com>
2026-01-15 06:39:06 +0530

    fix(web-client): add supabase domain to next.config.js image patterns

--- apps/web-client/next.config.js
+++ apps/web-client/next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'gdnltvvxiychrsdzenia.supabase.co',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
